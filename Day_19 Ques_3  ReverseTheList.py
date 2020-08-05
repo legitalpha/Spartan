4103ea954714998c6b16306be0bafbdcb593014f
Ravi Krishna Anand <33481637+legitalpha@users.noreply.github.com>
2020-08-05 13:56:22 +0530

    Add files via upload

---
+++ Day_19 Ques_3  ReverseTheList.py
@@ -0,0 +1,28 @@
+lst = []
+
+elementsInList = int(input("Enter the number of elements in the list : "))
+if elementsInList<=0:
+    print("Invalid Input")
+    exit()
+
+for i in range(0, elementsInList):
+    ele = int(input())
+    lst.append(ele)
+
+print(lst)
+
+def reverseList(lst):
+
+    newList = lst[-1]
+    newList = []
+    for ele in lst:
+        newList.append(ele)
+    newList.reverse()
+    print(newList)
+
+    if lst==newList:
+        print(True)
+    else:
+        print(False)
+
+reverseList(lst)
