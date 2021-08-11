03251e9412a686029e1b1b2afb669734b92560aa
Ravi Krishna Anand <33481637+legitalpha@users.noreply.github.com>
2021-08-11 10:41:26 +0530

    Update Josephus Problem.py

--- Josephus Problem.py
+++ Josephus Problem.py
@@ -1,5 +1,19 @@
-#This programme is only applicable when n(skipping sequence) = 2.
+#Josephus Problem using recursion
 
+def josephus(n, k):
+    if (n == 1):
+        return 1
+    else:
+        return (josephus(n - 1, k) + k - 1) % n + 1
+
+n = int(input("Enter the number of people in the circle: "))
+k = int(input("Skipping sequence: "))
+
+print("The chosen place is ", josephus(n, k))
+
+
+#This programme is only applicable when n(skipping sequence) = 2.
+#bitwise approach
 x = int(input("Enter a number : "))
 def josephusProblem(x):
     y = bin(x)
